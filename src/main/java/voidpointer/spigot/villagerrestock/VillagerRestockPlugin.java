3bfe85d2ad47f61ed71ff266b8521137552f97aa
Vasiliy <void.pointer@ya.ru>
2022-07-07 13:00:13 +0500

    Initial commit

---
+++ src/main/java/voidpointer/spigot/villagerrestock/VillagerRestockPlugin.java
@@ -0,0 +1,17 @@
+package voidpointer.spigot.villagerrestock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VillagerRestockPlugin extends JavaPlugin {
+    @Override public void onLoad() {
+
+    }
+
+    @Override public void onEnable() {
+
+    }
+
+    @Override public void onDisable() {
+
+    }
+}
